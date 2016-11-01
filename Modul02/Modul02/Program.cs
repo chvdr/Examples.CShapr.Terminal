fbd90ddeefe1b8cdae4478a6ecfd88c71e71ed4e
chvdr <chvdr@abv.bg>
2016-11-02 00:22:12 +0200

    Create program --Hello Aula! --

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// Variable declarations
+
+			// Add numbers
+
+			// Writing into a console
+
+		}
+	}
+}
