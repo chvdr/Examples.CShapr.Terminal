7862440bba70d25e86d719e462a3b6791b417e16
chvdr <chvdr@abv.bg>
2016-11-02 10:46:24 +0200

    Add program that calculates and displays result of a+b, where a and b are fixed integers (variables of type int)

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -7,11 +7,22 @@ namespace Modul02
 		public static void Main(string[] args)
 		{
 			// Variable declarations
+			int a = 0; int b = 0;		// numbers to be added
+			int sum = 0;                // variable w. the sum of the above vars
 
 			// Add numbers
+			a = 6;
+			b = 12;
+
+			sum = a + b;
 
 			// Writing into a console
-
+			Console.Write("a=");
+			Console.WriteLine(a);
+			Console.Write("b=");
+			Console.WriteLine(b);
+			Console.Write("sum=");
+			Console.WriteLine(sum);
 		}
 	}
 }
