f117760982a18e24c44ef223d74cbac717dac47c
chvdr <chvdr@abv.bg>
2016-11-08 13:14:19 +0200

    Use concatenation (sum) of strings

--- Modul02/ReadFromCommandLine/Program.cs
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -19,8 +19,19 @@ namespace ReadFromCommandLine
 			Console.WriteLine("Example of how to use cl input: ");
 			Console.Write(a + " + " + b + " = " );
 			Console.WriteLine((a + b).ToString());
-			// 
-			// 
+
+			// Main operations with strings:
+			Console.WriteLine("\n\n\n");
+			Console.WriteLine("And now, let's try with concatenation: ");
+
+			string test = "1";
+			test += ", 2";
+			test += ", 3";
+			test += ", 4";
+			test += ", 5";
+			test += ", 6";
+
+			Console.WriteLine(test);
 		}
 	}
 }
