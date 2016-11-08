a2ad7f4046349faa6a46c63177827f87d5893be8
chvdr <chvdr@abv.bg>
2016-11-08 12:22:22 +0200

    Add readline commands

---
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int a = 5, b = 0;
+			Console.WriteLine("Example of how to use cl input: ");
+			Console.Write(a + " + " + b + " = " );
+			Console.WriteLine(a + b);
+			// 
+			// 
+		}
+	}
+}
