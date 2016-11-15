1ebb4476129ea3989b30f2017d3a4ab6c4348a25
chvdr <chvdr@abv.bg>
2016-11-15 18:52:35 +0200

    Add TableProject: MODUL 02, Video 07 - Working with arrays

---
+++ Modul02/Modul02/TableProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TableProject
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string[] row = new string[3];
+
+			row[0] = "Line 1"; row[1] = "Line 2"; row[2] = "Line 3";
+
+			Console.WriteLine( "Here is the array: " + row[0] + ", " + row[1] + ", " + row[2]);
+
+			string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
+
+			Console.WriteLine("The power of the string " + "1,2,3,4,5,6,7,8,9" + " is:" + parse.Length );
+
+			string list1 = string.Join(";", parse);
+
+			Console.WriteLine( "New preformatted string: " + list1);
+
+		}
+	}
+}
