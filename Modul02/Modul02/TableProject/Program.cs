aa60b79b092274e36a183d0930a0c3803717ef72
chvdr <chvdr@abv.bg>
2016-11-16 20:21:31 +0200

    Add exercises with playing w. strings and ONE DIMENTIONAL arrays. Note: two dimentional will be in the next commit.

--- Modul02/Modul02/TableProject/Program.cs
+++ Modul02/Modul02/TableProject/Program.cs
@@ -6,20 +6,39 @@ namespace TableProject
 	{
 		public static void Main(string[] args)
 		{
-			string[] row = new string[3];
+			// [2,3] - dimentional table
+			// declaration of the 2 dimentional array
+			string[,] table = new string[2, 3];
 
-			row[0] = "Line 1"; row[1] = "Line 2"; row[2] = "Line 3";
+			// Name Surname Age
+			// Name Surname Age
 
-			Console.WriteLine( "Here is the array: " + row[0] + ", " + row[1] + ", " + row[2]);
+			// First line
+			table[0, 0] = "Chavdar"; table[0, 1] = "Georgiev"; table[0, 2] = "45";
 
-			string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
+			// second line
+			table[1, 0] = "Marin"; table[1, 1] = "Chilich"; table[1, 2] = "32";
 
-			Console.WriteLine("The power of the string " + "1,2,3,4,5,6,7,8,9" + " is:" + parse.Length );
-
-			string list1 = string.Join(";", parse);
-
-			Console.WriteLine( "New preformatted string: " + list1);
+			Console.WriteLine( table[0, 0][0] + ". " + table[0, 1] + ", " + table[0, 2] + " years old" );
+			Console.WriteLine(table[1, 0][0] + ". " + table[1, 1] + ", " + table[1, 2] + " years old");
 
 		}
 	}
 }
+
+
+
+
+//string[] row = new string[3];
+
+//row[0] = "Line 1"; row[1] = "Line 2"; row[2] = "Line 3";
+
+//Console.WriteLine( "Here is the array: " + row[0] + ", " + row[1] + ", " + row[2]);
+
+//string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
+
+//Console.WriteLine("The power of the string " + "1,2,3,4,5,6,7,8,9" + " is:" + parse.Length );
+
+//string list1 = string.Join(";", parse);
+
+//Console.WriteLine( "New preformatted string: " + list1);
---
+++ Modul02/Modul02/TableProjectRepetition/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TableProjectRepetition
+{
+	class MainClass
+	{
+		/*
+		 * The Project name:
+		 * TableProjectRepetition
+		 * 
+		 * This file is created only to repeat impressions of the last few lessons. Main TOPICS:
+		 * 1. Arrays
+		 * 2. Strings
+		 * 3. Strings as an arrays
+		 * 4. Two dimention arrays (also known as tables - this gave us name of the project)
+		*/ 
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hi! This is TableProjectRepetition, the work file for some features to be re-regard.");
+			Console.WriteLine("\n\n\n");
+			Console.WriteLine("1. Split and Parse in strings (arrays): ");
+			Console.WriteLine("\n");
+			Console.WriteLine("Let us assume row[] is 1-dimentional string with value as follows: \n 1,2,3,4,5,6,7,8,9");
+			Console.WriteLine("The task is to replace it with 1;2;3;4;5;6;7;8;9 i.e. to move commas to semi-colons \n");
+
+			// init the array: Define it with string, using commas as a separator
+			Console.WriteLine("\n\n First of all, lets init the variable: \n string parse = 1,2,3,4,5,6,7,8,9 \n\n");
+			// use Split to divide the string above to array of symbols 1 2 3 4 5 6 7 8 9 
+			Console.WriteLine("divide the string above to array of symbols 1 2 3 4 5 6 7 8 9");
+			Console.WriteLine(" Use \n string[] parse = \"1,2,3,4,5,6,7,8,9\".Split(','); \n to do that: \n\n");
+
+			string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');  
+
+			// use Lenght - 
+			Console.WriteLine("The defined string is " + parse.Length + " - characters long. \n This is also called Power of the array/string... \n\n" );
+
+			// replace commas w. semi-colons: 
+			string list1 = String.Join(";", parse);
+			Console.WriteLine("And here is the string (preformatted): " + list1 + "\n\n\n\n" );
+
+
+
+		}
+	}
+}
